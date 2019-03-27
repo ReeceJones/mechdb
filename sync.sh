2d300e70f3d2c5670ede83672942d24a1699b7bf
Simon <kartsims@gmail.com>
2019-03-27 10:27:20 +0100

    dirty fix for weird docker behavior in production

--- sync.sh
+++ sync.sh
@@ -8,4 +8,4 @@ rsync -rav --exclude node_modules ./api/ mechdb:/app/api/
 rsync -rav --exclude node_modules ./web/ mechdb:/app/web/
 rsync docker-compose.production.yml mechdb:/app/docker-compose.yml
 
-ssh mechdb "cd /app && docker-compose up -d && docker-compose restart api web"
+ssh mechdb "cd /app && docker-compose up -d && docker-compose restart api web && docker-compose restart proxy"
