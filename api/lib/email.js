f0b4776339904109b59ec2007bd35efa67b4cf63
Amy Dang <kirontoo@gmail.com>
2019-03-30 19:55:24 -0700

    moved mailgun api and domain to an env file.

--- api/lib/email.js
+++ api/lib/email.js
@@ -1,6 +1,4 @@
-const apiKey = '510685527f593bc26bad7bcde883e34c-de7062c6-100ae3e9'
-const domain = 'sandbox440de853a2404d95ad5abf85995566a4.mailgun.org'
-const mailgun = require('mailgun-js')({ apiKey, domain })
+const mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API, domain: process.env.MAILGUN_DOMAIN })
 
 module.exports = {
   send: async (data) => {
