396a7f87f357e03b1a4a5785732454fe987ec66a
Reece Jones <reece_jones@icloud.com>
2019-04-06 14:32:06 -0700

    add mobile support

--- web/components/details/Keyboard.vue
+++ web/components/details/Keyboard.vue
@@ -75,184 +75,187 @@
       </div>
     </div>
 
-    <div class="columns main">
+    <div class="columns">
       <div
         v-if="item.text"
-        class="quill-content column is-four-fifths"
+        class="quill-content column is-four-fifths-desktop is-full-mobile is-three-fifths-tablet"
         v-html="item.text"
       />
-      <table class="table details column">
-        <tbody>
+      <div class="column is-full-mobile">
+        <table class="table details is-fullwidth">
+          <tbody>
 
-          <tr v-if="item.switches.length || item.keycaps || item.cable || item.pcb || item.firmware || item.interface || item.dimensions || item.weight">
-            <th colspan="2">Specs</th>
-          </tr>
+            <tr v-if="item.switches.length || item.keycaps || item.cable || item.pcb || item.firmware || item.interface || item.dimensions || item.weight">
+              <th colspan="2">Specs</th>
+            </tr>
 
-          <tr v-if="item.switches.length">
-            <td>Switch options:</td>
-            <td>
-              <span
-                v-for="sw in item.switches"
-                :key="sw._id"
-              >
-                <nuxt-link
-                  :to="'/switches/' + sw.slug"
-                  class="button is-text"
+            <tr v-if="item.switches.length">
+              <td>Switch options:</td>
+              <td>
+                <span
+                  v-for="sw in item.switches"
+                  :key="sw._id"
                 >
-                  {{ sw.name }}
-                </nuxt-link>
-              </span>
-            </td>
-          </tr>
-          <tr v-if="item.keycaps">
-            <td>Default Keycaps:</td>
-            <td>
-              {{ item.keycaps }}
-            </td>
-          </tr>
-          <tr v-if="item.cable">
-            <td>Cable:</td>
-            <td>
-              {{ item.cable }}
-            </td>
-          </tr>
-          <tr v-if="item.pcb">
-            <td>PCB:</td>
-            <td>
-              {{ item.pcb }}
-            </td>
-          </tr>
-          <tr v-if="item.firmware">
-            <td>Firmware:</td>
-            <td>
-              {{ item.firmware }}
-            </td>
-          </tr>
-          <tr v-if="item.interface">
-            <td>Interface:</td>
-            <td>
-              {{ item.interface }}
-            </td>
-          </tr>
-          <tr v-if="item.dimensions">
-            <td>Dimensions:</td>
-            <td>
-              {{ item.dimensions }}
-            </td>
-          </tr>
-          <tr v-if="item.weight">
-            <td>Weight:</td>
-            <td>
-              {{ item.weight }}
-            </td>
-          </tr>
+                  <nuxt-link
+                    :to="'/switches/' + sw.slug"
+                    class="button is-text"
+                  >
+                    {{ sw.name }}
+                  </nuxt-link>
+                </span>
+              </td>
+            </tr>
+            <tr v-if="item.keycaps">
+              <td>Default Keycaps:</td>
+              <td>
+                {{ item.keycaps }}
+              </td>
+            </tr>
+            <tr v-if="item.cable">
+              <td>Cable:</td>
+              <td>
+                {{ item.cable }}
+              </td>
+            </tr>
+            <tr v-if="item.pcb">
+              <td>PCB:</td>
+              <td>
+                {{ item.pcb }}
+              </td>
+            </tr>
+            <tr v-if="item.firmware">
+              <td>Firmware:</td>
+              <td>
+                {{ item.firmware }}
+              </td>
+            </tr>
+            <tr v-if="item.interface">
+              <td>Interface:</td>
+              <td>
+                {{ item.interface }}
+              </td>
+            </tr>
+            <tr v-if="item.dimensions">
+              <td>Dimensions:</td>
+              <td>
+                {{ item.dimensions }}
+              </td>
+            </tr>
+            <tr v-if="item.weight">
+              <td>Weight:</td>
+              <td>
+                {{ item.weight }}
+              </td>
+            </tr>
 
-          <tr v-if="item.programmable !== null || item.simultaneousInput !== null || item.bluetooth !== null || item.hotswappable !== null || item.backlighting !== null || item.rgb !== null">
-            <th colspan="2">Features</th>
-          </tr>
+            <tr v-if="item.programmable !== null || item.simultaneousInput !== null || item.bluetooth !== null || item.hotswappable !== null || item.backlighting !== null || item.rgb !== null">
+              <th colspan="2">Features</th>
+            </tr>
 
-          <tr v-if="item.programmable !== null">
-            <td>Programmable:</td>
-            <td>
-              {{ item.programmable }}
-            </td>
-          </tr>
-          <tr v-if="item.simultaneousInput !== null">
-            <td>Simultaneous Key Input:</td>
-            <td>
-              {{ item.simultaneousInput ? 'Yes' : 'No' }}
-            </td>
-          </tr>
-          <tr v-if="item.bluetooth !== null">
-            <td>Bluetooth:</td>
-            <td>
-              {{ item.bluetooth ? 'Yes' : 'No' }}
-            </td>
-          </tr>
-          <tr v-if="item.hotswappable !== null">
-            <td>Hotswappable:</td>
-            <td>
-              {{ item.hotswappable ? 'Yes' : 'No' }}
-            </td>
-          </tr>
-          <tr v-if="item.backlighting !== null">
-            <td>Backlighting:</td>
-            <td>
-              {{ item.backlighting ? 'Yes' : 'No' }}
-            </td>
-          </tr>
-          <tr v-if="item.rgb !== null">
-            <td>RGB:</td>
-            <td>
-              {{ item.rgb ? 'Yes' : 'No' }}
-            </td>
-          </tr>
+            <tr v-if="item.programmable !== null">
+              <td>Programmable:</td>
+              <td>
+                {{ item.programmable }}
+              </td>
+            </tr>
+            <tr v-if="item.simultaneousInput !== null">
+              <td>Simultaneous Key Input:</td>
+              <td>
+                {{ item.simultaneousInput ? 'Yes' : 'No' }}
+              </td>
+            </tr>
+            <tr v-if="item.bluetooth !== null">
+              <td>Bluetooth:</td>
+              <td>
+                {{ item.bluetooth ? 'Yes' : 'No' }}
+              </td>
+            </tr>
+            <tr v-if="item.hotswappable !== null">
+              <td>Hotswappable:</td>
+              <td>
+                {{ item.hotswappable ? 'Yes' : 'No' }}
+              </td>
+            </tr>
+            <tr v-if="item.backlighting !== null">
+              <td>Backlighting:</td>
+              <td>
+                {{ item.backlighting ? 'Yes' : 'No' }}
+              </td>
+            </tr>
+            <tr v-if="item.rgb !== null">
+              <td>RGB:</td>
+              <td>
+                {{ item.rgb ? 'Yes' : 'No' }}
+              </td>
+            </tr>
 
-          <tr v-if="item.keysLayout !== null || item.layout !== null || item.spacebarSize !== null">
-            <th colspan="2">Layout</th>
-          </tr>
+            <tr v-if="item.keysLayout !== null || item.layout !== null || item.spacebarSize !== null">
+              <th colspan="2">Layout</th>
+            </tr>
 
-          <tr v-if="item.keysLayout !== null">
-            <td>Keys Layout:</td>
-            <td>
-              {{ optionLabel('keysLayouts', item.keysLayout) }}
-            </td>
-          </tr>
-          <tr v-if="item.layout !== null">
-            <td>Layout:</td>
-            <td>
-              {{ optionLabel('layouts', item.layout) }}
-            </td>
-          </tr>
-          <tr v-if="item.spacebarSize !== null">
-            <td>Spacebar size:</td>
-            <td>
-              {{ item.spacebarSize }}
-            </td>
-          </tr>
+            <tr v-if="item.keysLayout !== null">
+              <td>Keys Layout:</td>
+              <td>
+                {{ optionLabel('keysLayouts', item.keysLayout) }}
+              </td>
+            </tr>
+            <tr v-if="item.layout !== null">
+              <td>Layout:</td>
+              <td>
+                {{ optionLabel('layouts', item.layout) }}
+              </td>
+            </tr>
+            <tr v-if="item.spacebarSize !== null">
+              <td>Spacebar size:</td>
+              <td>
+                {{ item.spacebarSize }}
+              </td>
+            </tr>
 
-          <tr v-if="item.angle !== null || item.usbPassthrough !== null">
-            <th colspan="2">Design</th>
-          </tr>
+            <tr v-if="item.angle !== null || item.usbPassthrough !== null">
+              <th colspan="2">Design</th>
+            </tr>
 
-          <tr v-if="item.angle !== null">
-            <td>Slope/Typing Angle:</td>
-            <td>
-              {{ item.angle }}°
-            </td>
-          </tr>
-          <tr v-if="item.usbPassthrough !== null">
-            <td>USB Passthrough:</td>
-            <td>
-              {{ item.usbPassthrough ? 'Yes' : 'No' }}
-            </td>
-          </tr>
+            <tr v-if="item.angle !== null">
+              <td>Slope/Typing Angle:</td>
+              <td>
+                {{ item.angle }}°
+              </td>
+            </tr>
+            <tr v-if="item.usbPassthrough !== null">
+              <td>USB Passthrough:</td>
+              <td>
+                {{ item.usbPassthrough ? 'Yes' : 'No' }}
+              </td>
+            </tr>
 
-          <tr v-if="item.caseMaterial !== null || item.plateMaterial !== null || item.unitsMade">
-            <th colspan="2">Manufacturing Details</th>
-          </tr>
+            <tr v-if="item.caseMaterial !== null || item.plateMaterial !== null || item.unitsMade">
+              <th colspan="2">Manufacturing Details</th>
+            </tr>
 
-          <tr v-if="item.caseMaterial !== null">
-            <td>Case Material:</td>
-            <td>
-              {{ item.caseMaterial }}
-            </td>
-          </tr>
-          <tr v-if="item.plateMaterial !== null">
-            <td>Plate Material:</td>
-            <td>
-              {{ item.plateMaterial }}
-            </td>
-          </tr>
-          <tr v-if="item.unitsMade">
-            <td>Units made:</td>
-            <td>
-              {{ item.unitsMade }}
-            </td>
-          </tr>
+            <tr v-if="item.caseMaterial !== null">
+              <td>Case Material:</td>
+              <td>
+                {{ item.caseMaterial }}
+              </td>
+            </tr>
+            <tr v-if="item.plateMaterial !== null">
+              <td>Plate Material:</td>
+              <td>
+                {{ item.plateMaterial }}
+              </td>
+            </tr>
+            <tr v-if="item.unitsMade">
+              <td>Units made:</td>
+              <td>
+                {{ item.unitsMade }}
+              </td>
+            </tr>
 
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
+
     </div>
     
 
