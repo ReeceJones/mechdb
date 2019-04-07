040f384bbbb2b6fd2385224dea9766c59c4a8328
Simon <kartsims@gmail.com>
2019-04-07 07:09:38 +0200

    moved apiError method to plugins/api.js

--- web/plugins/api.js
+++ web/plugins/api.js
@@ -9,3 +9,35 @@ const api = axios.create({
 
 Vue.api = api
 Vue.prototype.$api = api
+
+Vue.mixin({
+  methods: {
+    apiError (err, fieldError = {}) {
+      let message
+      if (typeof err === 'string') {
+        // simple string as parameter
+        message = err
+      } else if (err.response && err.response.data.error) {
+        // API standard error
+        message = err.response.data.error
+        // catch form-specific error
+        if (fieldError[message]) {
+          this.errors.add({
+            field: fieldError[message],
+            msg: message,
+          })
+          return
+        }
+      } else {
+        // default handler
+        message = '💥 There has been an error with the server'
+        console.error(err)
+      }
+      this.$toast.open({
+        message,
+        type: 'is-danger',
+        position: 'is-bottom',
+      })
+    },
+  },
+})
