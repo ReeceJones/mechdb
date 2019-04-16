08dc97cf9ff07054b0ac5d227cb90fcc9edca0f0
Simon <kartsims@gmail.com>
2019-04-16 14:27:03 +0200

    about page

--- web/pages/about.vue
+++ web/pages/about.vue
@@ -1,6 +1,48 @@
 <template>
-  <div>
+  <div class="container">
     <h1 class="is-size-4 bit">About MechDB</h1>
-    <wip/>
+
+    <h2>Mission Statement</h2>
+    <p>Our goal is to provide the tools and space necessary for the community to generate a user curated centralization of all useful mechanical keyboard information.</p>
+
+    <h2>Why MechDB.net?</h2>
+    <p>Having well curated, centralized information is useful to everyone. From the beginner trying to decide what their first board should be, to the creator trying to ensure their newest creation doesn't mirror a pre-existing one, it is something everyone can benefit from. </p>
+    <p>As it stands, however, information tends to be fragmented across several major hubs (r/mk, geekhack, deskthority, etc.), and the community as a whole has grown in size to the point where the velocity of new information being generated is beyond the ability of any one entity to track.</p>
+    <p>MechDB.net aims to combat this problem by providing the framework for user generated, highly query-able datasets. Instead of relying on one person or entity to track every new keyboard, keyset, or group buy, MechDB.net will allow users to not just browse and query, but add and update datasets themselves (with the help of some healthy systems and moderation to prevent chaos, of course).</p>
+
+    <h2>How it Works</h2>
+    <h3>Looking for Something?</h3>
+    <p>Select the database you'd like to browse and utilize the extensive set of filters to find exactly what you're looking for!</p>
+
+    <h3>Want to contribute?</h3>
+    <p>Sign up (it just takes an email!) and you'll be able to add new items or edits. Your first few additions will need to be approved by a moderator, but after 10 approved additions you will become a verified user and be able to add freely (though users who abuse this priviledge will be banned!).</p>
+
+    <blockquote>
+      Now go, explore, contribute, and above all else, keep on clacking!
+    </blockquote>
   </div>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+  padding: 0 0 1em;
+}
+h2 {
+  font-weight: bold;
+  font-size: 1.4em;
+  padding: 0 0 .4em;
+}
+h3 {
+  font-size: 1.2em;
+  padding: 0 0 .4em;
+}
+p {
+  padding-bottom: 1em;
+  text-align: justify;
+}
+blockquote {
+  padding: 1em 1.5em;
+  margin-top: 1em;
+  border-left: 3px solid $light;
+}
+</style>
